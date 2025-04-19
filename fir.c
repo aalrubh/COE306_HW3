46d947ded85ce451a103cc254bbd42cd00e73da5
aalrubh <aalrubh@hotmail.com>
2025-04-19 15:39:15 +0300

    Added writeCSV, Wave Generation and updated loadFile, still missing FIR filter and updating the main function

--- fir.c
+++ fir.c
@@ -1,51 +1,97 @@
-//
-// Created by Ali on 2025-04-18.
-//
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
 
-void loadFile(const char *filename, float* filter, int size) {
-  FILE *file = fopen(filename, "rb");
+void loadFile(float* filter);
+void generateSineWave(float* signal);
+void writeCSV(float* data);
 
-  if (!file) {
-    perror("Failed to open filter file");
-    exit(EXIT_FAILURE);
-  }
+/*
+    The parameters for the filter
+*/
+char* inputFile = "filter.bin";
+char* outputFile = "output.csv";
 
-  if (fread(filter, sizeof(float), size, file) != size) {
-    printf("Error reading the file");
-    fclose(file);
-    exit(EXIT_FAILURE);
-  }
-  fclose(file);
+float waveFrequency = 0.1;
+float waveAmplitude = 1.0;
+float sampleRate = 1.0;
+
+int size = 2000;
+
+int main() {
+    float* filter = (float*)malloc(size * sizeof(float));
+
+    if (!filter) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    float* signal = (float*)malloc(size * sizeof(float));
+
+    if (!signal) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    loadFile(filename, filter, size);
+
+    for (int i = 0; i < size; i++) {
+        printf("%f\n", filter[i]);
+    }
+
+    free(filter);
+    return 0;
 }
 
 /*
-implement an FIR filter (considering the sampling
-rate is 1 Hz, for simplicity). You may use the circular buffer implementaion to perform the
-convolution of the filter coefficients with the input signal
+    This function will be used to load the contents of filter binary file into an array.
 */
+void loadFile(float* filter) {
+    FILE *file = fopen(inputFile, "rb");
 
+    if (!file) {
+        perror("Failed to open filter file");
+        exit(EXIT_FAILURE);
+    }
 
-
-int main() {
-  char* filename = "filter.bin";
-  int size = 2000;
-
-  float* filter =(float*)malloc(size * sizeof(float));
-
-  if (!filter) {
-    perror("Memory allocation failed");
-    exit(EXIT_FAILURE);
-  }
-
-  loadFile(filename, filter, size);
-
-  for (int i = 0; i < size; i++) {
-    printf("%f\n", filter[i]);
-  }
-
-  free(filter);
-  return 0;
+    if (fread(filter, sizeof(float), size, file) != size) {
+        printf("Error reading the file");
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+    fclose(file);
 }
+
+/*
+    This function will be used to write the output values into a CSV file to use for plotting.
+*/
+void writeCSV(float* data) {
+
+    FILE *file = fopen(outputFile, "w");
+
+    if (!file) {
+        perror("Failed to open output file");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < size; i++) {
+        fprintf(file, "%f\n", data[i]);
+    }
+
+    fclose(file);
+}
+
+/*
+    This function will be used to generate a sine wave signal at a specific sampling rate.
+*/
+void generateSineWave(float* signal) {
+    for (int i = 0; i < size; i++) {
+        signal[i] = waveAmplitude * sin(2 * M_PI * waveFrequency * i / sampleRate);
+    }
+}
+
+
+
